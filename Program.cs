5d17cef62282f49f8a930f1ed7bd5501d09aa5ca
Joe <j.principe343@gmail.com>
2021-12-30 19:54:46 -0800

    Created README and initialized C# files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BAYCNFT_Downloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string assetContractAddress = "https://opensea.io/assets/0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d";
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+A simple program to save the ugly monkey NFTs made by Bored Ape Yacht Club
+Still figuring out this whole git and github thing, so we'll see how this all goes

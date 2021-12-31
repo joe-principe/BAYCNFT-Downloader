7fe576350cd2c10a417b22ec97dbd8b9b6303027
Joe <j.principe343@gmail.com>
2021-12-30 20:17:09 -0800

    Implemented Rest request for assets

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 
 namespace BAYCNFT_Downloader
 {
@@ -7,6 +8,32 @@ namespace BAYCNFT_Downloader
         static void Main(string[] args)
         {
             const string assetContractAddress = "https://opensea.io/assets/0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d";
+            int assetID = 0;
+            string uri = $"{assetContractAddress}/{assetID}";
+            bool status = CheckURLStatus("uri");
+
+            while (status == true)
+            {
+                var client = new RestClient("uri");
+                var request = new RestRequest(Method.GET);
+                IRestResponse response = client.Execute(request);
+            }
         }
+        public bool CheckURLStatus(string website)
+        {
+            try
+            {
+                var request = WebRequest.Create(website) as HttpWebRequest;
+                request.Method = "HEAD";
+                using (var response = (HttpWebResponse)request.GetResponse())
+                {
+                    return response.StatusCode == HttpStatusCode.OK;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }  
     }
 }
